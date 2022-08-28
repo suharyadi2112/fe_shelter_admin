4797fe5c7733e126425414e27f1e7dde5390617d
suharyadi <suharyadi21@outlook.com>
2022-08-28 14:10:24 +0700

    - update content

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Layout from '../components/Layout';
 function HomePage(){ 
   return (
       <Layout>
-        <div>Ini Halaman Home</div>
+        <div className="container-xxl flex-grow-1 container-p-y">
+          <h4 className="py-3 breadcrumb-wrapper mb-4">Page 1</h4>
+          <p>
+            Sample page.<br />For more layout options refer
+            <a
+              href="https://pixinvent.com/demo/frest-clean-bootstrap-admin-dashboard-template/documentation-bs5//layouts.html"
+              target="_blank"
+              className="fw-bold"
+              >Layout docs</a
+            >.
+          </p>
+        </div>
       </Layout>
     );
 }
