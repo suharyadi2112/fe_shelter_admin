86cced39676d1e3e5ff26a9b9d0894262d8942aa
suharyadi <suharyadi21@outlook.com>
2022-08-28 14:11:15 +0700

    - update header, footer, layout

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,22 @@
 export default function Footer(){
 	return( 
-		<p>Dev by Suharyadi</p>
+		// {/*<!-- Footer -->*/}
+        <footer className="content-footer footer bg-footer-theme">
+          <div className="container-fluid d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+            <div className="mb-2 mb-md-0">
+              ©
+              2022
+              , Edited to Next-JS by Suharyadi
+            </div>
+            <div>
+              <a
+                href="https://pixinvent.com/demo/frest-clean-bootstrap-admin-dashboard-template/documentation-bs5/"
+                target="_blank"
+                className="footer-link me-4"
+                >Documentation</a
+              >
+            </div>
+          </div>
+        </footer>
 	);
 }
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,20 +1,80 @@
-import Link from 'next/link'
-
 export default function Header(){
 	return( 
-		<header>
-			<ul>
-				<li>
-					<Link href="/">
-			          <a>Home</a>
-			        </Link>
-				</li>
-				<li>
-					<Link href="/about">
-			          <a>About</a>
-			        </Link>
-				</li>
-			</ul>
-		</header>
+		<nav className="layout-navbar navbar navbar-expand-xl align-items-center bg-navbar-theme" id="layout-navbar">
+            <div className="container-fluid">
+              <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+                <a className="nav-item nav-link px-0 me-xl-4" href="#">
+                  <i className="bx bx-menu bx-sm"></i>
+                </a>
+              </div>
+
+              <div className="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                <ul className="navbar-nav flex-row align-items-center ms-auto">
+                  {/*<!-- User -->*/}
+                  <li className="nav-item navbar-dropdown dropdown-user dropdown">
+                    {/*<a className="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">*/}
+                      <div className="avatar avatar-online">
+                        <img src="static/assets/img/avatars/1.png" alt="true" className="rounded-circle" />
+                      </div>
+                    {/*</a>*/}
+                    <ul className="dropdown-menu dropdown-menu-end">
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          <div className="d-flex">
+                            <div className="flex-shrink-0 me-3">
+                              <div className="avatar avatar-online">
+                                <img src="static/assets/img/avatars/1.png" alt="true" className="rounded-circle" />
+                              </div>
+                            </div>
+                            <div className="flex-grow-1">
+                              <span className="fw-semibold d-block lh-1">John Doe</span>
+                              <small>Admin</small>
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <div className="dropdown-divider"></div>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          <i className="bx bx-user me-2"></i>
+                          <span className="align-middle">My Profile</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          <i className="bx bx-cog me-2"></i>
+                          <span className="align-middle">Settings</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          <span className="d-flex align-items-center align-middle">
+                            <i className="flex-shrink-0 bx bx-credit-card me-2"></i>
+                            <span className="flex-grow-1 align-middle">Billing</span>
+                            <span className="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20"
+                              >4</span
+                            >
+                          </span>
+                        </a>
+                      </li>
+                      <li>
+                        <div className="dropdown-divider"></div>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          <i className="bx bx-power-off me-2"></i>
+                          <span className="align-middle">Log Out</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  {/*<!--/ User -->*/}
+                </ul>
+              </div>
+            </div>
+          </nav>
+
 	);
 }
