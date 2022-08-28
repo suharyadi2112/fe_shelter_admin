07dcdfc379ec69c1c2b25cfbbfce365204ce757e
suharyadi <suharyadi21@outlook.com>
2022-08-28 22:24:04 +0700

    - add color line hr

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Layout from '../components/Layout';
 
 function HomePage(){ 
+  const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 5
+        }}
+    />
+);
   return (
       <Layout>
         <div className="container-xxl flex-grow-1 container-p-y">
           <h4 className="py-3 breadcrumb-wrapper mb-4">Page 1</h4>
           <p>
-            Sample page.<br />For more layout options refer
+            Sample page.<br />Ini halaman page 1<br />
+          </p>
+          <ColoredLine />
+          <p>
             <a
               href="https://pixinvent.com/demo/frest-clean-bootstrap-admin-dashboard-template/documentation-bs5//layouts.html"
               target="_blank"
